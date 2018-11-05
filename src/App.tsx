475c999e9e8d42f86184531fe733a47525a8209e
Melanie Rush <melanierush@Melanies-MacBook-Pro.local>
2018-11-05 13:25:40 -0700

    added read me, license, and contributing files for Flex standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
+
+const sources = {
+  sintelTrailer: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+  bunnyTrailer: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+  bunnyMovie: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+  test: 'http://media.w3.org/2010/05/video/movie_300.webm',
+};
 
 class App extends Component {
+  state = {
+    played: 0,
+    loaded: 0,
+    playing: true,
+    url: null,
+    volume: 0.8,
+  }
+  // load = url => {
+  //   this.setState({
+  //     url,
+  //     played: 0,
+  //     loaded:0
+  //   })
+  // }
+
+  playPause () {
+   // this.setState({ playing: !this.state.playing })
+   console.log("hi")
+  }
+
+  onPlay () {
+   this.setState ({ playing: true })
+   console.log("play")
+
+  }
+
+  onPause() {
+   console.log("pause")
+
+  }
+
+  //<ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
+
   render() {
+    const { url, playing, volume, loaded, played } = this.state
+   // const SEPARATOR = ' . '
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div className ='app'>
+        <section className='section'>
+          <h1>ReactPlayer Demo</h1>
+          <div className='player-wrapper'>
+            <ReactPlayer
+                url = {sources.sintelTrailer} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                playing
+                volume = {volume}
+            />
+          </div>  
+        </section>      
+      <table>
+        <tbody>
+          <button onClick={this.playPause}> Play Pause Button</button>
+          <button onClick={this.onPause}> Pause</button>
+          <button onClick={this.onPlay}> Play</button>
+        </tbody>
+      </table>
+    </div>
+    ) 
   }
 }
+
 
 export default App;
