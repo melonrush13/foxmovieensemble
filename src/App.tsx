8496e93e194563fe8e0186d7b183822c2c9eef8d
Melanie Rush <melanierush@Melanies-MacBook-Pro.local>
2018-11-07 14:15:42 -0700

    cleaned up HTML elements to get rid of red flags

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactPlayer from 'react-player'
 
-
-const sources = {
+const movies = {
   sintelTrailer: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
   bunnyTrailer: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
   bunnyMovie: 'http://media.w3.org/2010/05/bunny/movie.mp4',
@@ -11,34 +10,30 @@ const sources = {
 };
 
 
-// var movies = [
-//     {url : "http://media.w3.org/2010/05/sintel/trailer.mp4", MovieName: "sintelTrailer"},
-//     {url : "http://media.w3.org/2010/05/bunny/trailer.mp4", MovieName: "bunnyTrailer"},
-//     {url : "http://media.w3.org/2010/05/bunny/movie.mp4", MovieName: "bunnyMovie"},
-//     {url: "http://media.w3.org/2010/05/bunny/movie.mp4", MovieName: "test"}
-// ];
 
-
-//used to define a type with properties
-//interface Iface { }
-
- 
 class App extends Component {
   state = {
     played: 0,
     loaded: 0,
     playing: true,
-    url: '',
-   // url: null,
+    url: '', // url: null,
     volume: 0.8,
     loop: true,
     duration: 0,
     playbackRate: 1.0,
-
+    loadedSeconds: 0,
+    playedSeconds: 0
   }
 
+
   playPause () {
-   console.log("hi")
+    if (this.state.playing == true) {
+      this.setState({ playing: false })
+    }
+    else {
+      this.setState({ playing: true })
+    }
+
   }
 
   onPlay = () => {
@@ -62,12 +57,31 @@ class App extends Component {
     this.setState({ url: r })
   }
   
-  onProgress = (state : {playedSeconds: number }) => {
-      console.log('onProgress', state)
+  onProgress = (state : {playedSeconds: number , loadedSeconds: number, played: number}) => {
+      console.log('onProgress ', state)
+      console.log("secs: " + state.playedSeconds);
+      console.log("loaded: " + state.loadedSeconds);
+      console.log("played: " + state.played);
+      
+      this.setState({loadedSeconds: state.loadedSeconds}); 
+      this.setState({playedSeconds: state.playedSeconds});
 
-    //TODO:
-    //frame number - num of boxes per frame, 
-    //draw # of those frames
+      // let played = state.played;
+      // let loaded = state.loadedSeconds;
+      // const secs = state.playedSeconds;
+      // let frames = secs * 60;
+
+      var canvas : any = this.refs.boundingboxcanvas
+      var ctx = canvas.getContext("2d");
+      ctx.fillStyle = "#FF0000";
+      ctx.fillRect(0,0,150,200);
+
+
+    }
+
+  boundingBoxClicked() {
+    console.log("video clicked")
+
   }
 
   render() {
@@ -75,79 +89,97 @@ class App extends Component {
 
     return (
       <div className ='app'>
-        <section className='section'>
-          <h1>ReactPlayer Demo</h1>
+        <section className='videoPlayer'>
+          <div id="title">
+            <h1>ReactPlayer Demo</h1>
+          </div>
           <div className='player-wrapper'>
-            <ReactPlayer
-                className = 'react-player'
-               // url = {sources.sintelTrailer} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                url = {url} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
-
-                playing = {playing}
-                volume = {volume}
-                height = '75%'
-                width = '75%'
-                playbackRate = {playbackRate}
-                onProgress = {this.onProgress}
-                //onDuration ={this.onDuration}
-            />
+            <div id="base">
+              <ReactPlayer
+                  className = 'react-player'
+                // url = {sources.sintelTrailer} //'https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                  url = {url} 
+                  playing = {playing}
+                  volume = {volume}
+                  playbackRate = {playbackRate}
+                  onProgress = {this.onProgress}
+              />
+            </div>
+            <div id="overlay">
+              <canvas ref="boundingboxcanvas" onClick={this.boundingBoxClicked}></canvas>
+            </div>
           </div>  
+          <h2>Settings</h2>
+          <table id="controls">
+            <tbody>
+              <tr>
+                <th>Controls</th>
+                <td>
+                  <button onClick={this.playPause}> Play Pause Button</button>
+                  <button onClick={this.onPause}> Pause</button>
+                  <button onClick={this.onPlay}> Play</button>
+                </td>
+              </tr>
+              <tr>
+                <th>Playback</th>
+                <td>
+                  <button onClick={()=> this.setPlaybackRate(.5)}>.5</button>
+                  <button onClick={()=> this.setPlaybackRate(1)}>1</button>
+                  <button onClick={() => this.setPlaybackRate(2)}>2</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          </section>
+  
+          <section className='section'>
+          <h2>Movies</h2>
+            <table> 
+              <tbody>
+                <tr>
+                  <th>Sintel Trailer</th>
+                  <td> 
+                    <button onClick={() => this.setMovieUrl(movies.sintelTrailer)}>Play</button>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Bunny Movie Trailer</th>
+                  <td>
+                    <button onClick={() => this.setMovieUrl(movies.bunnyTrailer)}>Play</button>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Bunny Movie</th>
+                  <td>
+                    <button onClick={() => this.setMovieUrl(movies.bunnyMovie)}>Play </button>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Deadpool Trailer</th>
+                  <td>
+                    <button onClick={() => this.setMovieUrl(movies.deadpool)}>Play</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+          <h2>State</h2>
+          <table id="time">
+            <tbody>
+              <tr>
+                <th>Seconds Ellapsed: </th>
+                <td> {this.state.playedSeconds} </td>
+              </tr>
+              <tr>
+                <th>Loaded:</th>
+                <td> {this.state.loadedSeconds} </td>
+              </tr>
+            </tbody>
+          </table>
         </section>      
-      <table>
-        <tbody>
-          <button onClick={this.playPause}> Play Pause Button</button>
-          <button onClick={this.onPause}> Pause</button>
-          <button onClick={this.onPlay}> Play</button>
-      
-        </tbody>
-        <tbody>
-          <tr>
-            <td>
-            <h6>Playback Speed</h6>
-              <button onClick={()=> this.setPlaybackRate(.5)}>.5</button>
-              <button onClick={()=> this.setPlaybackRate(1)}>1</button>
-              <button onClick={() => this.setPlaybackRate(2)}>2</button>
-            </td>
-          </tr>
-        </tbody>
-        <h2>State </h2>
-        <tbody>
-          <tr>
-            <th>Seconds Elapsed:</th>
-            <td></td>
-          </tr>
-          <tr>
-            <th>Time Remaining:</th>
-            <td>{played}</td>
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      <section className='section'>
-        <table>
-          <tbody>
-            <tr> 
-              <th>Sintel Trailer</th>
-              <button onClick={() => this.setMovieUrl(sources.sintelTrailer)}>Watch</button>
-            </tr>
-            <tr>
-              <th>Bunny Movie Trailer</th>
-              <button onClick={() => this.setMovieUrl(sources.bunnyTrailer)}>Watch</button>
-            </tr>
-            <tr>
-              <th>Bunny Movie</th>
-              <button onClick={() => this.setMovieUrl(sources.bunnyMovie)}>Watch </button>
-            </tr> <tr>
-              <th>Deadpool Trailer</th>
-              <button onClick={() => this.setMovieUrl(sources.deadpool)}>Watch</button>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    </div>
+      </div>
     ) 
   }
 }
-
 
 export default App;
