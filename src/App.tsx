98235806750be667d05891a280df882a2844d6d7
Evan Louie <evan.louie@microsoft.com>
2018-12-03 15:18:42 -0800

    sound prediction icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { stringToRGBA } from "./colour";
 // import "./App.css";
 import ReactPlayer from "react-player";
-import { Stage, Layer, Rect, Transformer } from "react-konva";
+import { Stage, Layer, Rect, Transformer, Path } from "react-konva";
 
 //todo: have display one second before and one second
 //add color from evan's code, returns rgb string and defaults to opacity
@@ -206,16 +206,13 @@ class App extends React.Component<IMedia, IAppState> {
                   })}
                   {currentAudioPredictions.map(prediction => {
                     return (
-                      <Rect
+                      <Path
+                        fill={stringToRGBA(prediction.classifier)}
                         key={JSON.stringify(prediction)}
-                        x={0}
-                        y={0}
-                        width={videoWidth}
-                        height={videoHeight}
-                        name={prediction.classifier}
-                        fill={stringToRGBA(prediction.classifier, {
-                          alpha: 0.5
-                        })}
+                        x={20}
+                        y={20}
+                        scale={{ x: 5, y: 5 }}
+                        data="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.76 16.24l-1.41 1.41C4.78 16.1 4 14.05 4 12c0-2.05.78-4.1 2.34-5.66l1.41 1.41C6.59 8.93 6 10.46 6 12s.59 3.07 1.76 4.24zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm5.66 1.66l-1.41-1.41C17.41 15.07 18 13.54 18 12s-.59-3.07-1.76-4.24l1.41-1.41C19.22 7.9 20 9.95 20 12c0 2.05-.78 4.1-2.34 5.66zM12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
                       />
                     );
                   })}
