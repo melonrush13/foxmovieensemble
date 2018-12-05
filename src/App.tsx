6677b31b267fca8cffb51834739707cca4c544d9
Evan Louie <evan.louie@microsoft.com>
2018-12-05 13:20:23 -0800

    make sourceUrl optional

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { secondsToTime } from "./time";
 
 export interface IMedia {
   title: string;
-  sourceUrl: string; // URL to either video, image, or audio file
+  sourceUrl?: string; // URL to either video, image, or audio file
   predictions: Array<IVideoPrediction | IAudioPrediction>;
 }
 
@@ -34,15 +34,16 @@ interface IAppState {
   categories: string[];
   predictionsByTime: { [seconds: number]: IPrediction[] | undefined };
   currentPlaybackTime: number;
-  peakInstance: Peaks.PeaksInstance | null;
+  peakInstance?: Peaks.PeaksInstance;
+  sourceUrl?: string;
 }
 
 class App extends React.Component<IMedia, IAppState> {
   public state: IAppState = {
-    peakInstance: null,
     currentPlaybackTime: 0,
     volume: 0.8,
     playbackRate: 1.0,
+    sourceUrl: this.props.sourceUrl,
     categories: Object.keys(
       this.props.predictions.reduce(
         (categories, { classifier }) => ({ ...categories, [classifier]: true }),
@@ -69,37 +70,54 @@ class App extends React.Component<IMedia, IAppState> {
     super(props);
   }
 
-  public componentDidMount() {
-    const audioPredictions = this.props.predictions.filter(
-      p => "time" in p && "duration" in p
-    ) as IAudioPrediction[];
-
-    const audioSegments = audioPredictions.map(p => {
-      return {
-        startTime: p.time / 1000,
-        endTime: p.time + p.duration / 1000,
-        color: stringToRGBA(p.classifier),
-        labelText: p.classifier
-      };
-    });
-    const options: Peaks.PeaksOptions = {
-      container: this.peaksContainerRef.current as HTMLElement,
-      mediaElement: this.peaksAudioRef.current as Element,
-      audioContext: new AudioContext(),
-      pointMarkerColor: "#006eb0",
-      showPlayheadTime: true
-      // segments: audioSegments
-    };
-    const peakInstance = Peaks.init(options);
-
-    this.setState({ peakInstance });
-  }
-
   public componentDidUpdate() {
     // Ensure the first label in always in view
     this.currentlyPlayingRefs.slice(0, 1).forEach(el => {
       el.scrollIntoView({ block: "center" });
     });
+
+    // Init peaks
+    const peaksContainer = this.peaksContainerRef.current;
+    const peaksMedia = this.peaksAudioRef.current;
+    if (!this.state.peakInstance && peaksContainer && peaksMedia) {
+      const { predictions } = this.props;
+      const audioPredictions = predictions.filter(
+        p => "time" in p && "duration" in p
+      ) as IAudioPrediction[];
+      const videoPredictions = predictions.filter(
+        ({ x, y, width, height, time }: any) =>
+          x && y && width && height && time
+      ) as IVideoPrediction[];
+
+      const audioSegments = audioPredictions.map(p => {
+        return {
+          startTime: p.time / 1000,
+          endTime: p.time + p.duration / 1000,
+          color: stringToRGBA(p.classifier),
+          labelText: p.classifier
+        };
+      });
+
+      const videoPoints = videoPredictions.map(p => {
+        return {
+          time: p.time / 1000,
+          labelText: p.classifier
+        };
+      });
+
+      const options: Peaks.PeaksOptions = {
+        container: this.peaksContainerRef.current as HTMLElement,
+        mediaElement: this.peaksAudioRef.current as Element,
+        audioContext: new AudioContext(),
+        pointMarkerColor: "#006eb0",
+        showPlayheadTime: true,
+        segments: audioSegments,
+        points: videoPoints
+      };
+      const peakInstance = Peaks.init(options);
+
+      this.setState({ peakInstance });
+    }
   }
 
   public onSeek = (newTime: number) => {
@@ -124,9 +142,10 @@ class App extends React.Component<IMedia, IAppState> {
       volume,
       playbackRate,
       predictionsByTime,
-      currentPlaybackTime
+      currentPlaybackTime,
+      sourceUrl
     } = this.state;
-    const { title, predictions, sourceUrl } = this.props;
+    const { title, predictions } = this.props;
 
     const reactPlayer = this.playerRef.current;
     const duration = (reactPlayer && reactPlayer.getDuration()) || -1;
@@ -181,113 +200,111 @@ class App extends React.Component<IMedia, IAppState> {
             className="video-player"
             style={{ border: "1px dotted grey", flex: "1" }}
           >
-            <div className="player-video" style={{ position: "relative" }}>
-              <ReactPlayer
-                ref={this.playerRef}
-                url={sourceUrl}
-                controls={true}
-                volume={volume}
-                playbackRate={playbackRate}
-                onProgress={({ playedSeconds }) => {
-                  this.setState({ currentPlaybackTime: playedSeconds });
+            {/* If sourceUrl provided, present visualizations. Otherwise, show <input /> */}
+            {sourceUrl ? (
+              <div
+                className="visualizations"
+                style={{
+                  height: "100%",
+                  maxHeight: "100%",
+                  overflowY: "scroll"
                 }}
-                progressInterval={250}
-                onSeek={this.onSeek}
-                onPause={() => {
-                  const { peakInstance } = this.state;
-                  if (peakInstance) {
-                    peakInstance.player.pause();
-                  }
-                }}
-                onPlay={() => {
-                  const { peakInstance } = this.state;
-                  if (peakInstance) {
-                    peakInstance.player.play();
+              >
+                <div className="player-video" style={{ position: "relative" }}>
+                  <ReactPlayer
+                    ref={this.playerRef}
+                    url={sourceUrl}
+                    controls={true}
+                    volume={volume}
+                    playbackRate={playbackRate}
+                    onProgress={({ playedSeconds }) => {
+                      this.setState({ currentPlaybackTime: playedSeconds });
+                    }}
+
+                    progressInterval={250}
+                    onSeek={this.onSeek}
+                    onPause={() => {
+                      const { peakInstance } = this.state;
+                      if (peakInstance) {
+                        peakInstance.player.pause();
+                      }
+                    }}
+                    onPlay={() => {
+                      const { peakInstance } = this.state;
+                      if (peakInstance) {
+                        peakInstance.player.play();
+                      }
+                    }}
+                  />
+                  <Stage
+                    width={videoWidth}
+                    height={videoHeight}
+                    scale={{ x: scaleX, y: scaleY }}
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      maxWidth: "100%",
+                      maxHeight: "100%",
+                      overflow: "hidden", // Generated canvas will be videoWidth x videoHeight even when scaled.
+                      pointerEvents: "none"
+                    }}
+                  >
+                    <Layer>
+                      {currentVideoPredictions.map(prediction => {
+                        return (
+                          <Rect
+                            key={JSON.stringify(prediction)}
+                            x={prediction.x}
+                            y={prediction.y}
+                            width={prediction.width}
+                            height={prediction.height}
+                            name={prediction.classifier}
+                            fill={stringToRGBA(prediction.classifier, {
+                              alpha: prediction.confidence / 100
+                            })}
+                            stroke="black"
+                          />
+                        );
+                      })}
+                      {currentAudioPredictions.map(prediction => {
+                        return (
+                          <Path
+                            fill={stringToRGBA(prediction.classifier, {
+                              alpha: prediction.confidence / 100
+                            })}
+                            key={JSON.stringify(prediction)}
+                            x={20}
+                            y={20}
+                            scale={{ x: 5, y: 5 }}
+                            data="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.76 16.24l-1.41 1.41C4.78 16.1 4 14.05 4 12c0-2.05.78-4.1 2.34-5.66l1.41 1.41C6.59 8.93 6 10.46 6 12s.59 3.07 1.76 4.24zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm5.66 1.66l-1.41-1.41C17.41 15.07 18 13.54 18 12s-.59-3.07-1.76-4.24l1.41-1.41C19.22 7.9 20 9.95 20 12c0 2.05-.78 4.1-2.34 5.66zM12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+                          />
+                        );
+                      })}
+                    </Layer>
+                  </Stage>
+                </div>
+                <div ref={this.peaksContainerRef} />
+                <audio ref={this.peaksAudioRef}>
+                  <source src={sourceUrl} type="audio/mpeg" />
+                </audio>
+              </div>
+            ) : (
+              // Allow user to input media from local filesystem
+              <input
+                type="file"
+                accept="video/*"
+                onChange={ev => {
+                  const { files } = ev.currentTarget;
+                  if (files) {
+                    const file = files[0];
+                    this.setState({
+                      sourceUrl: URL.createObjectURL(file)
+                    });
                   }
                 }}
               />
-              <Stage
-                width={videoWidth}
-                height={videoHeight}
-                scale={{ x: scaleX, y: scaleY }}
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  maxWidth: "100%",
-                  maxHeight: "100%",
-                  overflow: "hidden", // Generated canvas will be videoWidth x videoHeight even when scaled.
-                  pointerEvents: "none"
-                }}
-              >
-                <Layer>
-                  {currentVideoPredictions.map(prediction => {
-                    return (
-                      <Rect
-                        key={JSON.stringify(prediction)}
-                        x={prediction.x}
-                        y={prediction.y}
-                        width={prediction.width}
-                        height={prediction.height}
-                        name={prediction.classifier}
-                        fill={stringToRGBA(prediction.classifier, {
-                          alpha: prediction.confidence / 100
-                        })}
-                        stroke="black"
-                      />
-                    );
-                  })}
-                  {currentAudioPredictions.map(prediction => {
-                    return (
-                      <Path
-                        fill={stringToRGBA(prediction.classifier, {
-                          alpha: prediction.confidence / 100
-                        })}
-                        key={JSON.stringify(prediction)}
-                        x={20}
-                        y={20}
-                        scale={{ x: 5, y: 5 }}
-                        data="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.76 16.24l-1.41 1.41C4.78 16.1 4 14.05 4 12c0-2.05.78-4.1 2.34-5.66l1.41 1.41C6.59 8.93 6 10.46 6 12s.59 3.07 1.76 4.24zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm5.66 1.66l-1.41-1.41C17.41 15.07 18 13.54 18 12s-.59-3.07-1.76-4.24l1.41-1.41C19.22 7.9 20 9.95 20 12c0 2.05-.78 4.1-2.34 5.66zM12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
-                      />
-                    );
-                  })}
-                </Layer>
-              </Stage>
-            </div>
-            <div ref={this.peaksContainerRef} />
-            <audio ref={this.peaksAudioRef}>
-              <source src={this.props.sourceUrl} type="audio/mpeg" />
-            </audio>
-            <h2 id="settings">Settings</h2>
-            <table id="controls">
-              <tbody>
-                <tr>
-                  <th>Playback</th>
-                  <td>
-                    <button onClick={() => this.setPlaybackRate(0.5)}>
-                      .5x
-                    </button>
-                    <button onClick={() => this.setPlaybackRate(1)}>1x</button>
-                    <button onClick={() => this.setPlaybackRate(2)}>2x</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <h2>State</h2>
-            <table id="time">
-              <tbody>
-                <tr>
-                  <th>Seconds Elapsed: </th>
-                  <td>{currentPlaybackTime}</td>
-                </tr>
-                <tr>
-                  <th>Played</th>
-                  <td>
-                    <progress max={duration} value={currentPlaybackTime} />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            )}
           </section>
           <section
             style={{
